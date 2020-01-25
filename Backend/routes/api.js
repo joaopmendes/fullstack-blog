43be5d56793027b69fccaf9762a2f94f657e100c
joaopmendes <41806813+joaopmendes@users.noreply.github.com>
2020-01-25 16:42:56 +0000

    + Add [db-post-implementation] Added list, delete and create posts

--- Backend/routes/api.js
+++ Backend/routes/api.js
@@ -1,9 +1,18 @@
 const {Router} = require(`express`)
 const routes = Router()
 const AuthController = require("../Controllers/AuthController");
+const PostController = require("../Controllers/PostController");
 const requireAuth = require("../Midllewares/AuthMiddleware")
+
+// Authentication Routes
 routes.post("/register", AuthController.register);
-routes.get("/protectedRoute", requireAuth, (req, res) => {
-    return res.status(200).json({test: req.user})
-});
+routes.post("/login", AuthController.login);
+// routes.get("/protectedRoute", requireAuth, (req, res) => {
+//     return res.status(200).json({test: req.user})
+// });
+
+routes.get("/posts", PostController.index);
+routes.post("/posts", requireAuth, PostController.store);
+routes.delete("/posts/:id", requireAuth, PostController.destroy);
+
 module.exports = routes;
