ffab29c14d738fd00b296b43486fd9abebca0b33
joaopmendes <joaopedrolopesmendes@gmail.com>
2020-05-22 14:19:20 +0100

    *CHANGE Extracted notifications to its own responsabilities

--- frontend/src/create-router.config.jsx
+++ frontend/src/create-router.config.jsx
@@ -1,49 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
+import Notifications from './Components/Notifications/notifications';
 import NormalRoutes from './routes/normal.routes';
 import AdminRoutes from './routes/admin.routes';
-import { useSelector, useDispatch } from 'react-redux';
-import { MDBNotification } from 'mdbreact';
-import { removeNotification } from './Store/Notifications/notifications.actions';
-import Cliclable from './Components/MacroControllers/Clicable';
+
 const Routes = () => (
   <>
     <NormalRoutes />
     <AdminRoutes />
   </>
 );
-export const RouterConfig = () => {
-  const notifications = useSelector((store) => store.notifications);
-  const dispatch = useDispatch();
-  return (
-    <>
-      <BrowserRouter>
-        {notifications.map((notification) => (
-          <Cliclable
-            onClick={() => {
-              dispatch(removeNotification(notification.id));
-            }}
-          >
-            <MDBNotification
-              show
-              fade
-              iconClassName={`text-${notification.type}`}
-              title={`${notification.title}`}
-              message={`${notification.message}`}
-              style={{
-                position: 'fixed',
-                top: '100px',
-                right: '350px',
-                width: '200px',
-                zIndex: 9999,
-              }}
-            />
-          </Cliclable>
-        ))}
-        <Switch>
-          <Routes />
-        </Switch>
-      </BrowserRouter>
-    </>
-  );
-};
+export const RouterConfig = () => (
+  <>
+    <BrowserRouter>
+      <Notifications />
+      <Switch>
+        <Routes />
+      </Switch>
+    </BrowserRouter>
+  </>
+);
