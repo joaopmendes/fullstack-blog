74889a0f96cc23a7a9c68c86bdedba0789ca748c
joaopmendes <joaopedrolopesmendes@gmail.com>
2020-05-24 20:00:22 +0100

    +ADD Support to express serve react files

--- Backend/index.js
+++ Backend/index.js
@@ -3,18 +3,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes/api");
-
-
+const path = require("path");
 const app = express();
 // Public Folder
-app.use("/public", express.static('./public'));
-app.use(bodyParser.urlencoded())
-app.use(express.json())
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded());
+app.use(express.json());
 app.use("/api", routes);
-mongoose.connect(process.env.MONGOOSE_URL, {useNewUrlParser: true})
-    .then(ok => console.log("Connected to db"))
-    .catch(err => console.log(err));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
-
+mongoose
+  .connect(process.env.MONGOOSE_URL, { useNewUrlParser: true })
+  .then((ok) => console.log("Connected to db"))
+  .catch((err) => console.log(err));
 
 app.listen(process.env.PORT);
